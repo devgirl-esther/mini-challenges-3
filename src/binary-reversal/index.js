034371ff2b3039f3282702f5df7af2eff66c1ed3
Esther Ime Edoho <Starexedoho@gmail.com>
2022-11-30 22:10:54 +0100

    submission for challenge 3

--- src/binary-reversal/index.js
+++ src/binary-reversal/index.js
@@ -3,6 +3,26 @@
  *
  *  * @param {string} value
  */
-function binaryReversal(value) {}
-
+function binaryReversal(value) {
+    //account for if a user inputs a negative integer
+    let num = Number(value)
+    if(num < 0){
+        return "invalid"
+    }
+    //convert number to binary
+let binary = num.toString(2);
+console.log(binary)
+// let padBinary = '00'+ binary
+let padBinary = binary.padStart(8, 0);
+//let padBinary = padStart(3,"00")
+console.log(padBinary);
+//reverse the converted number
+let reversebinary = padBinary.split('').reverse();
+//join and then turn to DECIMAL number
+let binaryString = reversebinary.join('')
+let finalNumber = parseInt(binaryString,2)
+let final = `${finalNumber}`
+return final
+}
+console.log(binaryReversal("4567"))
 module.exports = binaryReversal;
